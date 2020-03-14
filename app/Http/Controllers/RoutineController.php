d7c3b0b7483d02b82f83a63cb20a606d35600df3
Jose Luis <joseluismunozzuniga@gmail.com>
2020-03-14 14:29:34 -0300

    arreglando conflictos con rama master

--- app/Http/Controllers/RoutineController.php
+++ app/Http/Controllers/RoutineController.php
@@ -1,19 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-<<<<<<< HEAD
-use App\Http\Controllers\Controller;
-use App\Routine;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use DB;
-use Input;
-use Storage;
-
-class RoutineController extends Controller
-{
-    /**
-=======
 
 use App\Routine;
 use Illuminate\Http\Request;
@@ -22,32 +9,14 @@ use App\Http\Controllers\ApiController;
 class RoutineController extends ApiController
 {
         /**
->>>>>>> master
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-<<<<<<< HEAD
-    public function index()
-    {
-        $routines=Routine::all();
-        return view('routine.index',compact('routines'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('routine.create');
-=======
     public function index(Request $request)
     {
         $routines = Routine::all();
         return $this->showAll($routines);
->>>>>>> master
     }
 
     /**
@@ -58,48 +27,6 @@ class RoutineController extends ApiController
      */
     public function store(Request $request)
     {
-<<<<<<< HEAD
-       // $dataroutine=$request->except('_token');
-        $message=["required"=>'El: atribute es requerido'];
-        $request->validate([
-            'title'=>'required',
-            'description'=>'required',
-            'duration'=>'required',
-            'frequency'=>'required'
-        ],$message);
-        
-        $Routine=new Routine([
-            'title'=>$request->get('title'),
-            'description'=>$request->get('description'),
-            'duration'=>$request->get('duration'),
-            'frequency'=>$request->get('frequency'),
-        ]);
-        $Routine->save();
-       return redirect('/routine')->with('message','Guardado Satisfactoriamente');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $routines = Routine::find($id);
-        return view('routine.edit',['routine'=>$routines]);
-=======
         // Recoger POST
         $rules = [
             'trainer_id' => 'required',
@@ -123,7 +50,6 @@ class RoutineController extends ApiController
     {
         $routine = Routine::findOrFail($id);
         return $this->showOne($routine);
->>>>>>> master
     }
 
     /**
@@ -135,26 +61,6 @@ class RoutineController extends ApiController
      */
     public function update(Request $request, $id)
     {
-<<<<<<< HEAD
-        $message=["required"=>'El: atribute es requerido'];
-        $request->validate([
-            'title'=>'required',
-            'description'=>'required',
-            'duration'=>'required',
-            'frequency'=>'required'
-        ],$message);
-        $routine=Routine::find($id);
-        $routine->title = $request->title;
-        $routine->description = $request->description;
-        $routine->duration = $request->duration;
-        $routine->frequency = $request->frequency;
-         
-        $routine->save();
-    
-       // Session::flash('message', 'Editado Satisfactoriamente !');
-        return redirect('routine')->with('message', 'Modificado Satisfactoriamente !');
-        
-=======
         $routine = Routine::findOrFail($id);
 
         $rules = [
@@ -201,27 +107,16 @@ class RoutineController extends ApiController
         return $this->showOne($routine);
     }
 
->>>>>>> master
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-<<<<<<< HEAD
-    }
-    public function destroy($id)
-
-    {
-        Routine::destroy($id);
-        return redirect('routine')->with('message', 'routine deleted!');
-
-=======
     public function destroy($id)
     {
         $routine = Routine::findOrFail($id);
         $routine->delete();
         return $this->showOne($routine);
->>>>>>> master
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,8 @@
 |
 */
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('home');
-});
-//Auth::routes(['register'=>false,'reset'=>false]);
-Auth::routes();
-Route::resource('athlete','AthleteController')->middleware( 'auth' );
-Route::resource('plan','PlanController')->middleware( 'auth' );
-Route::resource('profile','ProfileController')->middleware( 'auth' );
-Route::resource('trainer','TrainerController')->middleware( 'auth' );
-Route::resource('routine','RoutineController')->middleware( 'auth' );
-Route::resource('training','TrainingController')->middleware( 'auth' );
-
-
-
-=======
 
 /* 
 Route::get('/', function () {
     return "Api desde laravel";
 }); */
->>>>>>> master
