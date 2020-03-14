c413a34eb0bc945f9d427a130e62bbf47313fb3d
Jose Luis <joseluismunozzuniga@gmail.com>
2020-03-14 14:40:46 -0300

    conflicto en la nueva estructura del user

--- app/User.php
+++ app/User.php
@@ -54,46 +54,6 @@ class User extends Authenticatable
         return $this->hasOne('App\Trainer');
     }
 
-<<<<<<< HEAD
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class)->withTimestamps();
-    }
-
-    public function authorizeRoles($roles)
-    {
-        if($this->hasAnyRole($roles)){
-                    return true;
-
-        }
-        abort_unless($this->hasAnyRole($roles), 401);
-    }
-
-    public function hasAnyRole($roles)
-    {
-        if (is_array($roles)) {
-            foreach ($roles as $role) {
-                if ($this->hasRole($role)) {
-                    return true;
-                }
-            }
-        } else {
-            if ($this->hasRole($roles)) {
-                return true; 
-            }   
-        }
-        return false;
-    }
-    
-    public function hasRole($role)
-    {
-        if ($this->roles()->where('name', $role)->first()) {
-            return true;
-        }
-        return false;
-    }
-=======
->>>>>>> master
 
 
 }
