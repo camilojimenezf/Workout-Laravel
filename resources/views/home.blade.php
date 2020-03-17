640fa21c25be0cfac725b0ff5d431614c1dbef2f
Neliab <Neliab>
2020-03-16 21:52:55 -0300

    menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,51 +27,72 @@
 </div> -->
 
 <style type="text/css">
-    h1{
-        color: white;
-        text-align: center;
-        padding-top:5cm;
+    h1{ padding-top:5cm;
         font-weight: 700;
         font-size: 62px;}
-    h5 {
-        color: white;
-        text-align: center;
-        font-size: 20px;}
-    h6 {
-        color: black;
-        text-align: center;
-        font-size: 15px;}
-    h4{
-        padding-top:15cm;
-        color: black;
-        text-align: center;
-        font-size:40px;
-    }
-    .contenedor{
-    height: 49%;
-    margin: 1% 1%;
+.estilo{
+    color: white;
+    text-align: center;
 }
 
   </style>
 
 
-<body background="https://i.pinimg.com/originals/5e/01/b1/5e01b1851854094d48be8fd833055753.jpg" >
-    <div >
-        <div>
-            <div>
+<body background="https://wallpaperboat.com/wp-content/uploads/2019/06/workout-motivation-02.jpg" >
+    <div>
+        <div class="estilo">
+            <div >
                 <h1> NO PAIN NO GAIN </h1>
 
-                <h5> Having a perfect body requires a lot of training. Nice-looking body and <h5>
-                <h5> powerful organism are interconnected – and we can help you with both. </h5>
+                <h5 style="font-size:20px"> Having a perfect body requires a lot of training. Nice-looking body and <h5>
+                <h5 style="font-size:20px"> powerful organism are interconnected – and we can help you with both. </h5>
             </div>
-            <div class="contenedor">
-                <h4> <br> WELCOME</br> </h4> <hr>
-                <h6> Welcome to the website Intense Gym! We hope that you will appreciate our services and opportunities we offer </h6>
-                <h6> our loyal and potential customers. Here are some of them: </h6>
+
+            <a class="btn btn-danger btn-anis-effect fadeInUp animated" 
+            href="#welcome" data-caption-animate="fadeInUp" 
+            data-caption-delay="800"><span class="btn-text">get started</span></a>
+
+
+            <div >
+            
+                <h4 style="font-size:40px;padding-top:10cm"> <br id="welcome"> WELCOME</br> </h4> <hr>
+                
+                <h6 style="font-size: 15px"> Welcome to the website Intense Gym! We hope that you will appreciate our services and opportunities we offer </h6>
+                <h6 style="font-size: 15px"> our loyal and potential customers. Here are some of them: </h6>
 
             </div>  
-        </div>
-    </div>
+            <p style="padding-top:3cm"> </p>
+            <div class="row">
+                    <div class="col-md-1"> </div>
+                    <div class="col-md-3">
+                        <div class="box-icon box-icon-bordered">
+                            <h4 class="text-danger offset-top-20">Qualified Coaches</h4>
+                            <p>Our coaches have years of experience in  </p>
+                            <p>various types of fitness and sports.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="box-icon box-icon-bordered">
+                            <h4 class="text-danger offset-top-20">Individual Approach </h4>
+                            <p>Every client of Intense has a personalized  </p>
+                            <p>program of training and nutrition. </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="box-icon box-icon-bordered">
+                            <h4 class="text-danger offset-top-20">Modern Fitness Equipment</h4> 
+                            <p>We cooperate with leading fitness equipment  </p>
+                            <p>suppliers to give you the superior results. </p>
+                        </div>
+                    </div>
+                    <div class="col-md-1"> </div>
+            </div>
+           
+            <h4 style="font-size:40px;padding-top:10cm"> <br></br> </h4> <hr>
+        </div>  <!-- Div class estilo -->
+
    </div>
 </body>
 @endsection
+
+
